1faf88ae55556cfbf8c201dbce0e8547f9798d8b
kishorekumarm7295-ship-it <kishorekumar.m7295@gmail.com>
2026-09-03 07:25:55 +0530

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,8 @@
+@echo off
+title OmniMart Full-Stack E-Commerce Server
+echo =========================================================
+echo   Starting OmniMart Universal E-Commerce Platform...
+echo =========================================================
+echo.
+py server.py
+pause
